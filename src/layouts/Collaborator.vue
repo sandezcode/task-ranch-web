87be8d7e1886e75cdc46d40e5039d9065c7aa2eb
Gerardo Sánchez <51423594+sandezcode@users.noreply.github.com>
2025-08-15 17:41:19 -0700

    Create Collaborator.vue

---
+++ src/layouts/Collaborator.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-btn color="primary" append-icon="mdi-chevron-down" variant="outlined" rounded="lg" position="fixed"
+         location="top right"
+         class="ma-2"
+         style="z-index: 9999;"
+  >
+    Marcos Torres
+  </v-btn>
+
+  <v-main>
+    <v-row no-gutters>
+      <v-col cols="12" lg="8" class="mx-auto">
+        <router-view />
+      </v-col>
+    </v-row>
+  </v-main>
+</template>
+
+<script>
+export default {
+  name: 'Collaborator'
+}
+</script>
+
+<style scoped lang="sass">
+
+</style>
