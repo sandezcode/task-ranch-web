95237767519c94816cc823b76da93836627e34cb
Gerardo Sánchez <51423594+sandezcode@users.noreply.github.com>
2025-08-19 16:44:02 -0700

    Update Land.vue

--- src/layouts/Land.vue
+++ src/layouts/Land.vue
@@ -4,6 +4,11 @@
 
 <script>
 export default {
-  name: 'Land'
+  name: 'Land',
+  mounted(){
+    if(localStorage.getItem('current_app_theme')){
+      this.$vuetify.theme.global.name = localStorage.getItem('current_app_theme');
+    }
+  }
 }
 </script>
